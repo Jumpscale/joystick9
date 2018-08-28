ab9ccd4cd0dd0c9883da2f98f4a28b6012cc401d
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2018-08-28 09:50:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     license='Apache',
     packages=find_packages(),
     install_requires=[
-        'JumpScale9>=9.4.0',
+        'JumpScale9>=development',
         'paramiko>=2.2.3',  # for parallel-ssh
         'asyncssh>=1.9.0',
         'pymongo>=3.4.0',

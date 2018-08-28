05a95d1e9a2cb3b23b354bb249ddadbfc354c553
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2018-08-28 09:32:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ except ImportError:
 
 setup(
     name='JumpScale9Prefab',
-    version='9.4.0',
+    version='development,
     description='Automation framework for cloud workloads remote sal, sal= system abstraction layer',
     long_description=long_description,
     url='https://github.com/Jumpscaler/prefab9',
